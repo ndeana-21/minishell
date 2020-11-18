01cf554450ce80ad89925189b7fb7bbb84f2f209
ndeana-21 <ndeana@student.21-school.ru>
2020-11-18 21:27:01 +0300

    fix ms_echo

--- srcs/ms_echo.c
+++ srcs/ms_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gselyse <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ndeana <ndeana@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 16:34:58 by gselyse           #+#    #+#             */
-/*   Updated: 2020/11/16 17:13:55 by gselyse          ###   ########.fr       */
+/*   Updated: 2020/11/18 21:26:03 by ndeana           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,16 @@
 
 void		ms_echo(char *param)
 {
-	int i;
-	int flag;
+	int 	flag;
 
-	i = 1;
 	flag = 0;
-	while (param[i] != NULL)
+	if (ft_strlen("-n") == ft_strcmp(param, "-n"))
 	{
-		if (ft_strpass(param[i], "-n") && i == 1)
-			flag = 1;
-		else
-		{
-			ft_putstr_fd(param[i], 1);
-			if (param[i + 1] != NULL)
-				ft_putstr_fd(" ", 1);
-		}
-		i++;
+		flag = 1;
+		param += 2;
+		param = ft_strpass(param, " ");
 	}
+	ft_putstr_fd(param, 1);
 	if (!flag)
-		ft_putchar_fd("\n", 1);
+		ft_putchar_fd('\n', 1);
 }
